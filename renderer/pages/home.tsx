8104ba2776df3a8900b1d56ad8c0df2c390e0209
leandrofelix-dev <lehinfo.felix@gmail.com>
2024-01-12 21:47:33 -0300

    feat: iniciando boilerplate

---
+++ renderer/pages/home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function HomePage() {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Home</title>
+        <p>Tela inicial</p>
+      </Head>
+    </React.Fragment>
+  )
+}
