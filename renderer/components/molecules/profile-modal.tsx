906ba5f758304112ff87b05be7111429578663b5
Leandro Felix <contato@leandrofelix.dev.br>
2024-03-06 01:29:51 -0300

    feat: definindo localStorage

--- renderer/components/molecules/profile-modal.tsx
+++ renderer/components/molecules/profile-modal.tsx
@@ -2,8 +2,9 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { Button } from '../atoms/button'
 import { FaDoorOpen } from 'react-icons/fa'
+import { useEffect, useState } from 'react'
 
-const isLogged = true
+import dotenv from 'dotenv'
 
 interface ProfileModalProps {
   isOpen: boolean;
@@ -13,16 +14,28 @@ interface ProfileModalProps {
 }
 
 function ProfileModal({ isOpen, onClose, name, role }: ProfileModalProps) {
+  dotenv.config()
+  const [isLogged, setIsLogged] = useState(false)
+
+  const token = global.localStorage.getItem('isLogged')
+
+  useEffect(() => {
+    if (token) setIsLogged(true)
+  }, [token])
+
   return (
     <>
       {isOpen && (
         <div className="fixed inset-0 flex items-center justify-center z-50">
-          <div className="absolute bg-slate_900/30 w-full h-full top-0 left-0" onClick={onClose}></div>
+          <div
+            className="absolute bg-slate_900/30 w-full h-full top-0 left-0"
+            onClick={onClose}
+          ></div>
           <div className="absolute p-4 rounded-xl bg-slate_100 right-32 top-28 w-full max-w-[320px] max-h-40 z-50 gap-4 flex flex-col">
             <div className="flex justify-between flex-col gap-4">
               {isLogged ? (
                 <>
-                  <div className="flex items-center"> {/* Adicionado um wrapper flex */}
+                  <div className="flex items-center">
                     <div className="w-12 h-12 mr-4 overflow-hidden rounded-full">
                       <Image
                         src="/images/profile.png"
@@ -33,7 +46,9 @@ function ProfileModal({ isOpen, onClose, name, role }: ProfileModalProps) {
                     </div>
                     <div>
                       <p className="text-slate_900 text-lg font-bold">{`Olá, ${name}`}</p>
-                      <p className="text-slate_500 text-sm font-semibold">{role}</p>
+                      <p className="text-slate_500 text-sm font-semibold">
+                        {role}
+                      </p>
                     </div>
                   </div>
                   <div>
@@ -45,7 +60,7 @@ function ProfileModal({ isOpen, onClose, name, role }: ProfileModalProps) {
                 </>
               ) : (
                 <div>
-                  <div className='flex gap-2'>
+                  <div className="flex gap-2">
                     <Button variant={'primary'}>
                       <Link href="/login">Faça o login</Link>
                     </Button>
